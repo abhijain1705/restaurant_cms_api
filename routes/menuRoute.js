f30a525858dd055a3d549a03ae2695a75253dfd4
Abhi Jain <abhijain3002@gmail.com>
2023-03-15 21:24:54 +0530

    removed conecpt of saving image to server

--- routes/menuRoute.js
+++ routes/menuRoute.js
@@ -1,50 +1,11 @@
 const express = require("express");
 const routes = express.Router();
 const { addItem, removeItem, getRestaurantMenu } = require("../controllers/menuController");
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
 
-function removeImage(req, res, next) {
-    const { imageName } = req.body;
-
-    fs.unlink(`./public/uploads/${imageName}`, (err) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ error: 'Failed to delete image' });
-        }
-
-        console.log(`Image ${imageName} deleted successfully`);
-        next();
-    });
-}
-
-// Set up Multer storage engine
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.join('./public/uploads'));
-    },
-    filename: function (req, file, cb) {
-        cb(null, `image-${Date.now()}. ${file.originalname}`);
-    }
-});
-
-const fileFilter = (req, file, cb) => {
-    const allowedFileTypes = ["image/jpeg", "image/pjg", "image/png"];
-    if (allowedFileTypes.includes(file.mimetype)) {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
-}
-
-// Create Multer middleware
-const upload = multer({ storage: storage, fileFilter: fileFilter });
-
-routes.post("/remove", removeImage, removeItem);
+routes.post("/remove", removeItem);
 routes.get('/restaurantMenu', getRestaurantMenu);
 
 // Add error handling middleware for the /addOrUpdate route
-routes.post("/addOrUpdate", upload.single('image'), addItem);
+routes.post("/addOrUpdate", addItem);
 
 module.exports = routes;
