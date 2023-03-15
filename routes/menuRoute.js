2ddfdc103bbfe9f91b784cb57c43bf4c231bd9b4
Abhi jain <91718608+abhijain2003@users.noreply.github.com>
2023-03-15 20:14:18 +0530

    Update menuRoute.js

--- routes/menuRoute.js
+++ routes/menuRoute.js
@@ -22,7 +22,7 @@ function removeImage(req, res, next) {
 // Set up Multer storage engine
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join('./public/uploads'));
+        cb(null, path.join(__dirname, './public/uploads'));
     },
     filename: function (req, file, cb) {
         cb(null, `image-${Date.now()}. ${file.originalname}`);
